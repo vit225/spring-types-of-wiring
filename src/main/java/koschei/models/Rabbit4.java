8f526c485f141a2e19835cb2a1beeb6fabec15fc
vit225 <vitaly1090@mail.ru>
2022-03-27 17:30:50 +1000

    commit

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -4,9 +4,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
+    private final Duck5 duck;
+
+    public Rabbit4(Duck5 duck) {
+        this.duck = duck;
+    }
 
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck.toString();
     }
 }
